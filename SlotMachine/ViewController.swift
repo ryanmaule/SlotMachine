4011b33fcb70f88eed0b946efb9a453cbdab6cba
ryan <ryan@RYPLM019-Ryan-Home.local>
2015-06-04 19:49:25 -0400

    Controller

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
+    let kMarginForView:CGFloat = 10.0
+    let kSixth:CGFloat = 1.0/6.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +26,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupContainerViews() {
+        self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
     }
 
 
